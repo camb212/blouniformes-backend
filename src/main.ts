e7f55a54bbcfb92e3a3e1569da16069d33788e67
Camb212 <camila.benavidez@ute.edu.ec>
2025-06-11 20:46:27 -0500

    Cambiar puerto

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3018);
 }
 bootstrap();
